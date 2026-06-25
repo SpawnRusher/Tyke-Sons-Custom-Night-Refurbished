169295062e8a4472bc237d895c21cb0f7572634a
SpawnRusher <SpawnRusherYT@gmail.com>
2026-06-25 16:33:40 -0400

    Fixed Phantom Chipomat's ID being set to Toy's

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -14,8 +14,6 @@ func _ready() -> void:
 	var tween = get_tree().create_tween()
 	tween.tween_property(fade,"self_modulate:a",0,0.5)
 	
-
-
 func _on_settings_button_button_down() -> void:
 	var tween = get_tree().create_tween()
 	tween.tween_property(camera,"position:y",720,0.2)
